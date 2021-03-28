500e22b2e73b84c7af66dc3c8aad34110ca3b597
Long Pham <phamvanlonghn@gmail.com>
2021-03-28 08:51:41 +0700

    Them page home 1

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/home', (req, res, next) => {
     res.send('Home page')
 })
 
+app.get('/home-1', (req, res, next) => {
+    res.send('Home page')
+})
+
 app.listen(5566, () => {
     console.log('server is running at 5566')
 })
